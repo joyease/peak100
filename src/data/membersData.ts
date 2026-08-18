acd1b82fa34eeb3a1898cb1c5dc3329bdb219064
Joyease <hermannhuang@gmail.com>
2026-08-18 17:08:17 +0800

    Update membersData.ts

--- src/data/membersData.ts
+++ src/data/membersData.ts
@@ -3,7 +3,7 @@ import { HikerProfile } from '../types';
 export const HIKER_ROSTER: HikerProfile[] = [
   {
     nickname: 'Hermann',
-    email: 'hermanntalk@gmail.com',
+    email: 'hermann@gmail.com',
     levelTitle: '小百岳全紀錄完登探險家',
     certId: 'TW-HERMANN-2026-099',
     finishDate: '2026-08-18',
@@ -44,30 +44,33 @@ export const HIKER_ROSTER: HikerProfile[] = [
     ]
   },
   {
-    nickname: '山野旅人',
-    email: 'mountain.traveler@gmail.com',
+    nickname: '文華',
+    email: 'hermannhuang@gmail.com',
     levelTitle: '小百岳進階健行勇者',
     certId: 'TW-100HILLS-2026-105',
     finishDate: '2026-04-08',
-    motto: '遠離塵囂，走入山林寻找最純粹的平靜。',
-    // 58 peaks completed
+    motto: '遠離塵囂，走入山林最純粹的平靜。',
+    // 73 peaks completed
     completedPeakIds: [
       1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-      21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-      41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58
+      22, 23, 24, 25, 26, 27, 28, 29, 30, 32, 33, 35, 36, 37, 38, 39, 40,
+      41, 42, 43, 44, 46, 48, 49, 51, 52, 54, 55, 56, 57, 58, 59, 60,
+      61, 62, 63, 64, 67, 68, 72, 73, 74, 75, 76, 77, 78, 80, 82, 83, 94, 96, 98, 100
     ]
   },
   {
-    nickname: '百岳阿梅',
-    email: 'may.hike@gmail.com',
+    nickname: '如娟',
+    email: 'ruchuan30@gmail.com',
     levelTitle: '小百岳精銳登山客',
     certId: 'TW-100HILLS-2026-188',
     finishDate: '2026-03-15',
     motto: '山不在高，有心則達。小百岳也是滿滿台灣情。',
-    // 40 peaks completed
+    // 73 peaks completed
     completedPeakIds: [
       1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-      21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40
+      22, 23, 24, 25, 26, 27, 28, 29, 30, 32, 33, 35, 36, 37, 38, 39, 40,
+      41, 42, 43, 44, 46, 48, 49, 51, 52, 54, 55, 56, 57, 58, 59, 60,
+      61, 62, 63, 64, 67, 68, 72, 73, 74, 75, 76, 77, 78, 80, 82, 83, 94, 96, 98, 100
     ]
   },
   {
